f4b08d6d50c3174ea9695369262806030e2e79b3
Ronnypetson <rsronnypetson4@gmail.com>
2018-12-23 15:05:18 -0200

    meta-encoding

--- 3dvae/3dvae.py
+++ 3dvae/3dvae.py
@@ -34,24 +34,19 @@ def get_batch(data):
 
 def get_encodings(frames,model):
     enc = []
-    num_batches = len(frames)//batch_size
-    remainder = len(frames)%batch_size
+    num_batches = (len(frames)+batch_size)//batch_size
     for i in range(num_batches):
-        b = frames[i*num_batches:(i+1)*num_batches]
+        b = frames[i*batch_size:(i+1)*batch_size]
         if len(b) > 0:
             enc += model.transformer(b).tolist()
-    #if remainder > 0:
-    #    b = frames[-remainder:]
-    #    enc += model.transformer(b).tolist()
-    return np.array([np.array(enc[i:i+32]).reshape([128,32,1]) for i in range(len(frames)-32)])
+    return np.array([np.array(enc[i:i+32]).reshape([128,32,1]) for i in range(len(frames)-31)])
 
-def get_meta_encodings():
+def train_meta_ae():
     frames = log_run()
     ae = VanillaAutoencoder([None,64,64,1], 1e-3, batch_size, latent_dim)
     encodings = get_encodings(frames,ae)
     ae.close_session()
 
-    print(encodings.shape)
     num_sample=len(encodings)
     meta_ae = MetaVanillaAutoencoder([None,128,32,1], 1e-3, batch_size, latent_dim, '/home/ronnypetson/models/meta_encoder')
     for epoch in range(100):
@@ -65,7 +60,26 @@ def get_meta_encodings():
         print('[Epoch {}] Loss: {}'.format(epoch, loss))
     print('Done!')
 
-def main():
+def decode_seq():
+    frames = log_run(32)
+    ae = VanillaAutoencoder([None,64,64,1], 1e-3, batch_size, latent_dim)
+    encodings = get_encodings(frames,ae) # [1,128,32,1]
+
+    meta_ae = MetaVanillaAutoencoder([None,128,32,1], 1e-3, batch_size, latent_dim, '/home/ronnypetson/models/meta_encoder')
+    meta_enc = meta_ae.transformer(encodings) # shape [1,128]
+    rec_enc = meta_ae.generator(meta_enc) # shape([1,128,32,1])
+    rec_frames = ae.generator(rec_enc.reshape([32,128])).reshape([32,64,64])
+
+    fig = plt.figure()
+    rec_imgs = np.empty((4*h,8*w))
+    for i in range(4):
+        for j in range(8):
+            rec_imgs[i*h:(i+1)*h,j*w:(j+1)*w] = rec_frames[8*i+j]
+    plt.imshow(rec_imgs, cmap='gray')
+    plt.savefig('rec_frames.png')
+    plt.close(fig)
+
+def train_ae():
     frames = log_run()
     num_sample = len(frames)
     model = None
@@ -106,6 +120,7 @@ def main():
     print('Done!')
 
 if __name__ == '__main__':
-    #main()
-    get_meta_encodings()
+    #train_ae()
+    #train_meta_ae()
+    decode_seq()
 
