bae651e46de4e958ef248e5fcf34d33134e54b72
Ronnypetson <rsronnypetson4@gmail.com>
2019-05-04 17:58:51 -0300

    save emb must change too a a a

--- 3dvae/pt/_mapper_.py
+++ 3dvae/pt/_mapper_.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     frames = np.concatenate(frames,axis=0).transpose(0,2,1)
     #abs_poses = [s for s in abs_poses] #[s[:-1] for s in abs_poses]
     rel_poses = [abs2relative(s,seq_len,1) for s in abs_poses]
-    rel_poses = np.concatenate(rel_poses,axis=0)[1:].transpose(0,2,1)
+    rel_poses = np.concatenate(rel_poses,axis=0)[4:].transpose(0,2,1)
     print(frames.shape,rel_poses.shape)
 
     device = torch.device(device)
