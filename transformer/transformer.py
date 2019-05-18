a3dd32fdbff47449d27ac663093471189a8783d4
Ronnypetson <rsronnypetson4@gmail.com>
2019-05-18 16:37:42 -0300

    decoder stack added

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -48,14 +48,17 @@ class AttentionHead(nn.Module):
         self.W_v = nn.Parameter(torch.zeros(d_model,d_model,requires_grad=True))
         self.sqrt_d = torch.tensor(np.sqrt(d_model))
 
-    def forward(self,x,mask_from=None):
+    def forward(self,x,mask_from=None,Q=None,K=None,V=None):
         #print(x.size())
-        Q = torch.bmm(self.W_q.unsqueeze(0).repeat(x.size(0),1,1),x)
+        if Q is None:
+            Q = torch.bmm(self.W_q.unsqueeze(0).repeat(x.size(0),1,1),x)
         #print(Q.size())
-        K = torch.bmm(self.W_k.unsqueeze(0).repeat(x.size(0),1,1),x)
+        if K is None:
+            K = torch.bmm(self.W_k.unsqueeze(0).repeat(x.size(0),1,1),x)
         #print(K.size())
-        V = torch.bmm(self.W_v.unsqueeze(0).repeat(x.size(0),1,1),x)
-        #print(V.size())
+        if V is None:
+            V = torch.bmm(self.W_v.unsqueeze(0).repeat(x.size(0),1,1),x)
+        #print(Q.size(),K.size())
         x = torch.bmm(Q.transpose(1,2),K)/self.sqrt_d
         #print(x.size())
         if mask_from is not None:
@@ -74,10 +77,15 @@ class MultiHeadAttention(nn.Module):
         # D, n_heads*D x n_heads*D, D
         self.W = nn.Parameter(torch.zeros(n_heads*d_model,d_model),requires_grad=True)
 
-    def forward(self,x,mask_from=None):
+    def forward(self,x,mask_from=None,Q=None,K=None,V=None):
         #print(x.size())
-        x,Q,K,V = zip(*[h(x,mask_from) for h in self.heads])
-        x,Q,K,V = [list(a) for a in [x,Q,K,V]]
+        if Q is None or K is None or V is None:
+            x,Q,K,V = zip(*[h(x,mask_from) for h in self.heads])
+            x,Q,K,V = [list(a) for a in [x,Q,K,V]]
+        else:
+            x,Q,_,_ = zip(*[self.heads[i](x,mask_from,Q[i],K[i],V[i])\
+                          for i in range(len(self.heads))])
+            x = list(x)
         #print(len(list(zip([h(x) for h in self.heads]))))
 
         x = torch.cat(x,dim=1)
@@ -95,9 +103,9 @@ class TransformerEncoder(nn.Module):
         self.fc1 = nn.Linear(d_model,4*d_model)
         self.fc2 = nn.Linear(4*d_model,d_model)
 
-    def forward(self,x,mask_from=None):
+    def forward(self,x):
         #print(x.size())
-        x_,Q,K,V = self.att(x,mask_from)
+        x_,Q,K,V = self.att(x)
         #print(x_.size())
         x = self.bn1(x+x_)
         size = x.size()
@@ -113,6 +121,36 @@ class TransformerEncoder(nn.Module):
         #print(x)
         return x,Q,K,V
 
+class TransformerDecoder(nn.Module):
+    def __init__(self,d_model,n_heads):
+        super().__init__()
+        self.att = MultiHeadAttention(d_model,n_heads)
+        self.bn1 = nn.BatchNorm1d(d_model)
+        self.enc_dec = MultiHeadAttention(d_model,n_heads)
+        self.bn2 = nn.BatchNorm1d(d_model)
+        self.fc1 = nn.Linear(d_model,4*d_model)
+        self.fc2 = nn.Linear(4*d_model,d_model)
+        self.bn3 = nn.BatchNorm1d(d_model)
+
+    def forward(self,x,mask_from,K,V):
+        x_,Q,_,_ = self.att(x,mask_from)
+        #print(x_.size())
+        x = self.bn1(x+x_)
+        size = x.size()
+        x_,_,_,_ = self.enc_dec(x,mask_from=None,Q=Q,K=K,V=V)
+        x = self.bn2(x+x_)
+        #print(size)
+        x = x.view(-1,x.size(1))
+        x_ = F.relu(self.fc1(x))
+        x_ = self.fc2(x_)
+        x = x.view(size)
+        x_ = x_.view(size)
+        #print(x_.size())
+        x = self.bn3(x+x_)
+        #print(x.size())
+        #print(x)
+        return x
+
 class TransformerEncoderStack(nn.Module):
     def __init__(self,d_model,n_heads,n_encoders):
         super().__init__()
@@ -121,18 +159,49 @@ class TransformerEncoderStack(nn.Module):
         for i in range(n_encoders):
             self.encs.append(TransformerEncoder(d_model,n_heads))
 
-    def forward(self,x,mask_from=None):
+    def forward(self,x):
         x = self.pos_enc(x)
         #print(x.size())
         for enc in self.encs[:-1]:
-            x = enc(x,mask_from)[0]
+            x = enc(x)[0]
             #print(x.size())
-        x,Q,K,V = self.encs[-1](x,mask_from)
+        x,Q,K,V = self.encs[-1](x)
         #print(x)
         return x,Q,K,V
 
+class TransformerDecoderStack(nn.Module):
+    def __init__(self,d_model,d_out,n_heads,n_decoders):
+        super().__init__()
+        self.d_model = d_model
+        self.d_out = d_out
+        self.fc1 = nn.Linear(d_out,d_model)
+        self.pos_enc = PositionalEncoder(d_model)
+        self.decs = nn.ModuleList([])
+        for i in range(n_decoders):
+            self.decs.append(TransformerDecoder(d_model,n_heads))
+        self.fc2 = nn.Linear(d_model,d_out)
+
+    def forward(self,x,mask_from,K,V):
+        size = (x.size(0),self.d_model,x.size(2))
+        x = x.view(-1,x.size(1))
+        x = F.relu(self.fc1(x))
+        x = x.view(size)
+        x = self.pos_enc(x)
+        print(x.size())
+        for dec in self.decs:
+            x = dec(x,mask_from,K,V)
+            print(x.size())
+        size = (x.size(0),self.d_out,x.size(2))
+        x = x.view(-1,x.size(1))
+        x = F.relu(self.fc2(x))
+        x = x.view(size)
+        #print(x)
+        return x
+
 if __name__ == '__main__':
-    model = TransformerEncoderStack(64,8,4)
+    model = TransformerDecoderStack(64,64,8,4)
+    #TransformerDecoder(64,8)
+    #TransformerEncoderStack(64,8,4)
     #TransformerEncoder(64,8)
     #MultiHeadAttention(64,8)
     #AttentionHead(64)
@@ -148,7 +217,9 @@ if __name__ == '__main__':
         optimizer.zero_grad()
         x = data[ids[i]]
         #y = data_y[ids[i]]
-        y_ = model(x,mask_from=4)[0]
+        K = [torch.zeros(64,64,128) for _ in range(8)]
+        V = [torch.ones(64,64,128) for _ in range(8)]
+        y_ = model(x,mask_from=4,K=K,V=V)
         loss = loss_fn(x,y_)
         print(loss.item())
         loss.backward()
