16cf73c55ace403ed0cd760563e2f7214673fbfb
Ronnypetson <rsronnypetson4@gmail.com>
2019-06-02 18:30:21 -0300

    gdajhs ghadj aaa aaa aaa sd fgh j h jg jhg j asd a sd fds aa

--- 3dvae/pt/misc/plotter.py
+++ 3dvae/pt/misc/plotter.py
@@ -166,8 +166,8 @@ def plot_eval(model,test_loader,seq_len,device='cuda:0'):
         data_y += y.cpu().detach().numpy().reshape(-1,12).tolist()
         rel_poses += y_.cpu().detach().numpy().reshape(-1,12).tolist()
 
-    rel_poses = np.array(rel_poses) #.transpose(0,2,1)
-    gt = np.array(data_y) #.transpose(0,2,1)
+    rel_poses = np.array(rel_poses[::seq_len]) #.transpose(0,2,1)
+    gt = np.array(data_y[::seq_len]) #.transpose(0,2,1)
     print(rel_poses.shape)
     abs_ = np.array(relative2abs(rel_poses,seq_len))
     pts_ = np.array(relative2abs(gt,seq_len))
