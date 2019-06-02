d14868b67df73a29137e9a1b3e9d74cf9dbb4eb8
Ronnypetson <rsronnypetson4@gmail.com>
2019-06-02 15:27:01 -0300

    gdajhs ghadj aaa aaa

--- 3dvae/pt/misc/plotter.py
+++ 3dvae/pt/misc/plotter.py
@@ -161,7 +161,7 @@ def plot_eval(model,test_loader,seq_len,device='cuda:0'):
     rel_poses = np.array(rel_poses) #.transpose(0,2,1)
     gt = np.array(data_y) #.transpose(0,2,1)
     print(rel_poses.shape)
-    abs_ = relative2abs(rel_poses,seq_len)
+    abs_ = np.array(relative2abs(rel_poses,seq_len))
     print(abs_.shape)
 
     pts = get_3d_points_t(rel_poses,seq_len,abs_)
