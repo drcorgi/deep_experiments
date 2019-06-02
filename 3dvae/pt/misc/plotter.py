edb133dc40aae7a7890e4d32731f3ea4bbf4e7ed
Ronnypetson <rsronnypetson4@gmail.com>
2019-06-02 13:58:08 -0300

    fi he hekfe e  aa aasda da aaa

--- 3dvae/pt/misc/plotter.py
+++ 3dvae/pt/misc/plotter.py
@@ -84,12 +84,11 @@ def relative2abs(rel_poses,wsize):
     abs_poses = poses[:wsize]
     for i in range(wsize,len(poses)):
         in_p = abs_poses[wsize*(i//wsize)-1]
-        #print(in_p.shape)
+        print(in_p)
         try:
             in_p = np.linalg.inv(in_p)
             abs_poses.append(np.matmul(in_p,poses[i]))
-        except numpy.linalg.LinAlgError:
-            print(in_p)
+        except np.linalg.LinAlgError:
             abs_poses.append(abs_poses[-1])
     abs_poses = [flat_homogen(p) for p in abs_poses]
     return abs_poses
